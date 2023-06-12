WIP hessian fix

hls4ml/hessian_analysis.py
@@ -222,32 +222,32 @@ def main(args):
     BIT_WIDTH = 8
     strategy = "sum"
     # Hessian model-wide sensitivity ranking
-    eigenvalues, eigenvectors = hess.top_k_eigenvalues(k=top_k, max_iter=500)
-    sanitized_evs = list() #the list contains k lists of eigenvectors
-    print("Eigenvectors")
-    for i in range(len(eigenvalues)):
-        print(f"Top {i+1} eigenvalue: {eigenvalues[i]}")
-    for i in range(top_k):
-        curr_evs = list()
-        # print(eigenvectors[i])
-        print(f"Eigenvector shape i: {np.array(eigenvectors[i]).shape}")
-        for j in range(len(eigenvectors[i])):
-            print(f"--Eigenvector shape j: {np.array(eigenvectors[i][j]).shape}")
-            if np.array(eigenvectors[i][j]).size > 32:
-                curr_evs.append(np.array(eigenvectors[i][j]))
-        sanitized_evs.append(np.array(curr_evs))
+    eigenvalues, eigenvectors = hess.top_k_eigenvalues(k=top_k, max_iter=500, rank_BN=False)
+    # sanitized_evs = list() #the list contains k lists of eigenvectors
+    # print("Eigenvectors")
+    # for i in range(len(eigenvalues)):
+    #     print(f"Top {i+1} eigenvalue: {eigenvalues[i]}")
+    # for i in range(top_k):
+    #     curr_evs = list()
+    #     # print(eigenvectors[i])
+    #     print(f"Eigenvector shape i: {np.array(eigenvectors[i]).shape}")
+    #     for j in range(len(eigenvectors[i])):
+    #         print(f"--Eigenvector shape j: {np.array(eigenvectors[i][j]).shape}")
+    #         if np.array(eigenvectors[i][j]).size > 32:
+    #             curr_evs.append(np.array(eigenvectors[i][j]))
+    #     sanitized_evs.append(np.array(curr_evs))
     
-    for i in range(top_k):
-        for j in range(len(sanitized_evs[i])):
-            print(f" Sanitized i,j {i},{j} = {sanitized_evs[i][j].shape}")
-        break
+    # for i in range(top_k):
+    #     for j in range(len(sanitized_evs[i])):
+    #         print(f" Sanitized i,j {i},{j} = {sanitized_evs[i][j].shape}")
+    #     break
 
     print(f'Hessian eigenvalue compute time: {time.time() - hess_start} seconds\n')
     # eigenvalues = None
     rank_start_time = time.time()
 
     param_ranking, param_scores = hess.hessian_ranking_general(
-        sanitized_evs, eigenvalues=eigenvalues, k=top_k, strategy=strategy, iter_by=1
+        eigenvectors, eigenvalues=eigenvalues, k=top_k, strategy=strategy, iter_by=1
     )
     # bitwise_rank, bitwise_scores = hess.rank_bits(param_scores, 5) # add m = 5 bits (doesn't work; TODO: delete)
     bitwise_rank = hess.convert_param_ranking_to_msb_bit_ranking(param_ranking, BIT_WIDTH)